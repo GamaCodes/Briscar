6b718b1f71c8c7b9cef8373d972f818fb8f6dda5
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-07-27 23:19:27 -0500

    loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from "./config.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-link">PROXIMAMENTE</p>
-        <p>Sitio en construcción</p>
+        <img src="/load.svg" className="app-image" alt="Briscar" />
+        <p>En construcción</p>
+        <p className="App-link">AGOSTO 2021</p>
       </header>
     </div>
   );
